328cf7129c7bd959a35df01511dd2b49915494cf
Vortecsmaster <vortecsmaster@vortecsla.com>
2024-04-28 12:06:34 -0500

    Test cases included
    Simple Unit testing
    Simple Properties Based Testing
    NotSoSimple ScriptContext construction testing.

--- nativetokens/testnet/mint.sh
+++ nativetokens/testnet/mint.sh
@@ -2,7 +2,7 @@ utxoin1="7f1409ec3e5861a6371a3b9a10117538177d11d3dbba056254d12b4b14699145#4"
 policyid=$(cat ak_eaCoins.pid)
 output="2200000000"
 tokenamount="100"
-tokenname=$(echo -n "B98aikenToken" | xxd -ps | tr -d '\n')
+tokenname="$(echo -n "B98aikenToken2" | xxd -ps | tr -d '\n')"
 mintingScript="./compiled/ak_eaCoins.uplc"
 collateral="e623894e267cf3d4553327108d566d61b01298a1b56363b260ffb5e768578913#0"
 signerPKH="2070f8488dd696b78a5f23e38d273550e43660526c4b19cba733b488"
@@ -21,15 +21,15 @@ cardano-cli transaction build \
   --mint "$tokenamount $policyid.$tokenname" \
   --mint-script-file $mintingScript \
   --mint-redeemer-file ./values/redeemer.json \
-  --out-file mintTx.body
+  --out-file mintTx.unsigned
 
-cardano-cli transaction sign \
-    --tx-body-file mintTx.body \
-    --signing-key-file ../../../Wallets/Adr12.skey \
-    --signing-key-file ../../../Wallets/Adr11.skey \
-    $PREVIEW \
-    --out-file mintTx.signed
+# cardano-cli transaction sign \
+#     --tx-body-file mintTx.unsigned \
+#     --signing-key-file ../../../Wallets/Adr12.skey \
+#     --signing-key-file ../../../Wallets/Adr11.skey \
+#     $PREVIEW \
+#     --out-file mintTx.signed
 
- cardano-cli transaction submit \
-    $PREVIEW \
-    --tx-file mintTx.signed
+#  cardano-cli transaction submit \
+#     $PREVIEW \
+#     --tx-file mintTx.signed
