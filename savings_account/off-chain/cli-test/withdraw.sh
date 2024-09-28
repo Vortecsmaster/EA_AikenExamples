3452a3b07a08f6f1ea4c2f34e88ab899cd52a55f
Vortecsmaster <vortecsmaster@vortecsla.com>
2024-09-28 01:30:45 -0500

    Scripts CLI completos
    Withdraw included.

--- savings_account/off-chain/cli-test/withdraw.sh
+++ savings_account/off-chain/cli-test/withdraw.sh
@@ -1,32 +1,30 @@
-script_utxo="9819324cebdae3a94eb2ef746485e17fdfa837446d6975956b831283cde6a4dd#0"
+script_utxo="2704c34e7fd0a6f739feba23f7d63d81aebc0fb3f2c8bb4133fcf9b353503be8#0"
 script_address=$(cat ../compiled/savings.addr) 
-user_utxo="dd46747e40d4da9edc77c7531eb84b8dc7729a84d8d0a58c1c46f4acb4944819#0"
-output="7300000000"
+output="5000000000"
 saverPKH=$(cat $HOME/Dev/Wallets/Adr08.pkh)
 collateralPKH=$(cat $HOME/Dev/Wallets/Adr07.pkh)
 collateral="4cbf990857530696a12b0062546a4b123ad0bef21c67562e32d03e3288bdcd7b#0"
+Adr08=$(cat $HOME/Dev/Wallets/Adr08.addr)
 
 cardano-cli conway transaction build \
   $PREVIEW \
   --tx-in $script_utxo \
   --tx-in-script-file ../compiled/savings.uplc \
   --tx-in-datum-file ../values/saver.json \
-  --tx-in-redeemer-file ../values/value1.json \
-  --tx-in $user_utxo \
-  --required-signer-hash $saverPKH \
+  --tx-in-redeemer-file ../values/value0.json \
+    --required-signer-hash $saverPKH \
   --tx-in-collateral $collateral \
-  --tx-out $script_address+$output \
-  --tx-out-datum-hash-file ../values/saver.json \
+  --tx-out $Adr08+$output \
   --change-address $nami3 \
-  --out-file another_deposit.unsigned
+  --out-file withdraw.unsigned
 
 cardano-cli conway transaction sign \
-    --tx-body-file another_deposit.unsigned \
+    --tx-body-file withdraw.unsigned \
     --signing-key-file $HOME/Dev/Wallets/Adr08.skey \
     --signing-key-file $HOME/Dev/Wallets/Adr07.skey \
     $PREVIEW \
-    --out-file another_deposit.signed
+    --out-file withdraw.signed
 
 cardano-cli conway transaction submit \
     $PREVIEW \
-    --tx-file another_deposit.signed
+    --tx-file withdraw.signed
